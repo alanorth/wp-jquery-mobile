52ed19313fdd572ac7094667276f554359320625
Alan Orth <alan.orth@gmail.com>
2011-05-31 17:14:50 -0400

    Update jQuery to latest stable, 1.6.1.

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 	<?php wp_head(); ?> 
 	
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-	<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
 </head>
 
