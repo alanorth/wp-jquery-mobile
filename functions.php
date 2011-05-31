4ddf624def92056094d7a647e61c9e34af7cf4ef
Alan Orth <alan.orth@gmail.com>
2011-05-31 17:20:50 -0400

    Comment out the "Webdev Cat" branding from the footer... I won't delete it, though, so he can still get some traffic from github commit logs!

--- functions.php
+++ functions.php
@@ -11,6 +11,6 @@ add_action('init', 'removeHeadLinks');
 remove_action('wp_head', 'wp_generator');
     
 function footer(){
-	echo '<div data-role="footer" class="ui-bar"><a href="http://www.webdevcat.com" data-role="button" data-icon="info">Theme by WebDevCat</a></div>';
+	//echo '<div data-role="footer" class="ui-bar"><a href="http://www.webdevcat.com" data-role="button" data-icon="info">Theme by WebDevCat</a></div>';
 } add_action('wp_footer', 'footer');
 ?>
